7390c484d78f51fb0b41af137d91c5b6614d6387
dobreraj <dobreraj@yahoo.com>
2013-07-20 20:21:35 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require ('fs');
+var indexHtml= fs.readFileSync('index.html');
 var express = require('express');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello Worlds 2');
+  response.send(indexHtml.toString());
 });
 
 var port = process.env.PORT || 5000;
